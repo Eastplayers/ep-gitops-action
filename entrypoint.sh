05e6bddcdc01a0a99b551614b382e00029b55e36
Phuc Phung <phuc.phung@eastplayers.io>
2021-08-12 17:37:43 +0700

    implement kubectl deploy action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,35 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+# echo "Hello $1"
+
+K8S_SERVER_URL=$1
+K8S_TOKEN=$2
+K8S_DEPLOYMENT=$3
+K8S_IMAGE_NAME=$4
+K8S_NAMESPACE=$5
+
+
+# time=$(date)
+# echo "::set-output name=time::$time"
+if test -z "$K8S_IMAGE_NAME"
+then
+  kubectl \
+    --insecure-skip-tls-verify \
+    --kubeconfig="/dev/null" \
+    --server=${K8S_SERVER_URL} \
+    --token=${K8S_TOKEN} \
+    set image deployment/${K8S_DEPLOYMENT} \
+    app=${K8S_IMAGE_NAME} \
+    -n ${K8S_NAMESPACE} \
+    --record
+else
+  kubectl \
+    --insecure-skip-tls-verify \
+    --kubeconfig="/dev/null" \
+    --server=${K8S_SERVER_URL} \
+    --token=${K8S_TOKEN} \
+    rollout \
+    restart \
+    deployment/${K8S_DEPLOYMENT} \
+    -n ${K8S_NAMESPACE} \
+fi
