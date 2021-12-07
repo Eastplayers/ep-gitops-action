4753acff99494e64892e4201889a3345387d157d
Phuc Phung <phungthienphuc18@gmail.com>
2021-12-07 21:47:20 +0700

    init script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,21 @@
 #!/bin/sh -l
 
-# echo "Hello $1"
 set -e
 
-K8S_SERVER_URL=$1
-K8S_TOKEN=$2
-K8S_DEPLOYMENT=$3
-K8S_IMAGE_NAME=$4
-K8S_NAMESPACE=$5
-K8S_CONTAINER_NAME=${6:-app}
+APP_NAME=$1
+REPO=$2
+PATH=$3
+DEST_NAMESPACE=$4
+DOCKER_IMAGE=$5
+PROJECT=$6
+REVISION=${7:-HEAD}
 
-KUBECTL="$(find /opt/bitnami/kubectl/bin/ -name kubectl)"
-
-
-time=$(date)
-echo "::set-output name=time::$time"
-if ! test -z "$K8S_IMAGE_NAME"
-then
-  RESULT="$($KUBECTL \
-    --insecure-skip-tls-verify \
-    --kubeconfig="/dev/null" \
-    --server=${K8S_SERVER_URL} \
-    --token=${K8S_TOKEN} \
-    set image deployment/${K8S_DEPLOYMENT} \
-    ${K8S_CONTAINER_NAME}=${K8S_IMAGE_NAME} \
-    -n ${K8S_NAMESPACE} \
-    --record)"
-  echo $RESULT
-else
-  RESULT_RESTART="$($KUBECTL \
-    --insecure-skip-tls-verify \
-    --kubeconfig="/dev/null" \
-    --server=${K8S_SERVER_URL} \
-    --token=${K8S_TOKEN} \
-    rollout \
-    restart \
-    deployment/${K8S_DEPLOYMENT} \
-    -n ${K8S_NAMESPACE})"
-fi
+argocd app create $APP_NAME \
+  --repo $REPO \
+  --path $PATH \
+  --dest-namespace $DEST_NAMESPACE \
+  --dest-server https://kubernetes.default.svc \
+  --kustomize-image $DOCKER_IMAGE \
+  --sync-policy auto \
+  --project default \
+  --revision $REVISION \
