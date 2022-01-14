3b0b532a859c8d1982eb1eb2a4ba603274941a29
Phuc Phung <phungthienphuc18@gmail.com>
2022-01-14 17:53:00 +0700

    feat: update matching string

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,17 +10,21 @@ DEPLOYMENT_DIR=$2
 
 IMAGE_REPO=$3
 BRANCH=${6:master}
+MATCH=$7
 
 GIT_REPO_URL="https://pepepot:$GIT_TOKEN@$GIT_REPO"
 
 git clone $GIT_REPO_URL
+
 cd hsv-ops
 
-sed -i "s/0.1.*/0.1.$TAG/g" $DEPLOYMENT_DIR
+ls -la
+sed -i "s/$MATCH/$TAG/g" $DEPLOYMENT_DIR
 
 git config --global user.name "pepepot"
 git config --global user.email "ops@eastplayers.io"
 git config --global color.ui true
+
 git add $DEPLOYMENT_DIR
 git commit -m "[ci] PROD Update $IMAGE_REPO to $TAG"
 git push origin $BRANCH
